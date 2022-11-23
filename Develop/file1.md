5f856385aad84ee9ac2bc0e821d65e3474dae06e
Paul York <paulyork@Pauls-MacBook-Pro.local>
2022-11-23 10:28:11 -0800

    added template

---
+++ Develop/file1.md
@@ -0,0 +1,21 @@
+# file1
+
+* [Installation](#installation)
+* [Usage](#useinfo)
+* [Contribution](#contribution)
+* [Test](#test)
+* [License](#license)
+# Installation
+ok
+## Usage
+ok
+## Contribution
+ok
+## Test Info
+ok
+## License
+MIT
+
+# Contact
+* GitHub :ok
+* Email :py
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,15 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const fs =require('fs');
+const fs = require('fs');
+// const readmeContent = require('./generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [
+inquirer.prompt([
     {
         type: 'input',
         name: 'title',
         message: 'Enter the title of the project:',
+        validate: (value)=>{ if(value){return true} else {return 'Give the project a title to continue'}}
     },
     {
         type: 'input',
@@ -26,7 +28,7 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'contguide',
+        name: 'contribution',
         message: 'Enter contribution guidelines:',
     },
     {
@@ -35,8 +37,14 @@ const questions = [
         message: 'Enter test instructions:',
     },
     {
+        type: 'list',
+        name: 'license',
+        message: 'Select a license type:',
+        choices: ['MIT', 'GPL', 'Apache', 'GNU', 'N/A'],
+    },
+    {
         type: 'input',
-        name: 'username',
+        name: 'git',
         message: 'Enter your GitHub username:',
     },
     {
@@ -44,10 +52,47 @@ const questions = [
         name: 'email',
         message: 'Enter your email address:',
     },
-];
+]).then(({
+    title,
+    description,
+    install,
+    useinfo,
+    contribution,
+    test,
+    license,
+    git,
+    email,
+})=>{
+const template = `# ${title}
+
+* [Installation](#install)
+* [Usage](#useinfo)
+* [Contribution](#contribution)
+* [Test](#test)
+* [License](#license)
+# Installation
+${install}
+## Usage
+${useinfo}
+## Contribution
+${contribution}
+## Test Info
+${test}
+## License
+${license}
+
+# Contact
+* GitHub :${git}
+* Email :${email}`;
+
+writeToFile(title, template);
+}
+);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFileSync(`./${fileName.toLowerCase().split(' ').join('')}.md`, data)
+};
 
 // TODO: Create a function to initialize app
 function init() {}
